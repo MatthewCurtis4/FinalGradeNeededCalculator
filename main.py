67ce24507cfebb282c5835d20b0183ea22ab0819
MatthewCurtis4 <mcurtisbusinesses@gmail.com>
2020-11-19 11:58:13 -0500

    Tested and working fine, currently adding in an array and loop to go through all of the possible number marks to get

--- main.py
+++ main.py
@@ -2,13 +2,8 @@
 
 def get_marks():
 
-    minimum_12 = 90
-    minimum_11 = 85
-    minimum_10 = 80
-    minimum_9 = 77
-    minimum_8 = 73
-    minimum_7 = 70   
-    minimum_6 = 67
+    percent_to_get = [90, 85, 80, 77, 73, 70, 67]
+
     original_ask = input("How many entry marks do you have")
     weight_taken= 0
     weight_lost = 0
@@ -18,13 +13,15 @@ def get_marks():
         percentage = input("What percentage did you get? (0-100)")
         realpercentage = int(percentage)/100
         weight_lost += int(weight)*(1-realpercentage)
-    exam_weight = input("What is the weight of your final exam? (please enter answer like 20 if its worth 20%)")  
+    exam_weight = int(input("What is the weight of your final exam? (please enter answer like 20 if its worth 20%)"))
     if (exam_weight + weight_taken != 100):
         print ("Your weight completed so far plus your exam weight does not add up to 100")
         print ("To complete this calculation, we will assume you score the same on all remaining grades as you have so far this semester")
-    print ("Your current percentage going into exam is " + (weight_taken - weight_lost)/weight_taken)
-
-    print ("To finish the course with a 12 you would need" +   + "% on your final exam")
-        
+    current_percentage = (weight_taken - weight_lost)/weight_taken
+    print ("Your current percentage going into exam is ", current_percentage*100, "%")
+    ratio_to_scale = (100 - exam_weight)/weight_taken
+    real_weight_lost = ratio_to_scale*weight_lost
+    print ("To finish the course with a 12 you would need",  (exam_weight - ((100 - minimum_12) - real_weight_lost))/exam_weight, "% on your final exam")
+    print ("To finish the course with a 11 you would need",  (exam_weight - ((100 - minimum_11) - real_weight_lost))/exam_weight, "% on your final exam")        
      
-print (weight_lost)
+get_marks()
